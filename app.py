245aa651e5f1a65e0ecde0e28a641224b1a198dd
HanaHelaly <hanahelaly99@gmail.com>
2024-04-26 02:26:46 +0300

    Add the main file

---
+++ app.py
@@ -0,0 +1,76 @@
+from flask import Flask, render_template, request
+import base64
+from tensorflow.keras.preprocessing.image import load_img, img_to_array, array_to_img
+from tensorflow.keras.models import load_model
+import numpy as np
+from io import BytesIO
+from PIL import UnidentifiedImageError
+
+labels = {0: 'dew',
+ 1: 'fog smog',
+ 2: 'frost',
+ 3: 'glaze',
+ 4: 'hail',
+ 5: 'lightning',
+ 6: 'rain',
+ 7: 'rainbow',
+ 8: 'rime',
+ 9: 'sandstorm',
+ 10: 'snow'}
+
+# Initialize Flask application
+app = Flask(__name__)
+
+# Load the weights from the h5 file
+model = load_model('./model/FINAL_EfficientNetB7-201.h5')
+allowed_extensions = ['.jpg', '.jpeg', '.png']
+def preprocess_image(image):
+    # Load the image with target size of (380, 380), required by EfficientNetB4
+    image = image.resize((224, 224))
+    # Convert the image to a numpy array
+    image = img_to_array(image)
+    # Expand the dimensions to create a batch of size 1
+    image = np.expand_dims(image, axis=0)
+    # Rescale the image pixels
+    image = image/255.
+    return image
+
+# Define route for home page
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Define route for prediction
+@app.route('/predict', methods=['POST'])
+def predict():
+    # Get the image file from the request
+    file = request.files['image']
+    # Check if the file has an allowed extension
+    if not any(file.filename.lower().endswith(ext) for ext in allowed_extensions):
+        allowed_extensions_str = ', '.join(allowed_extensions)
+        return render_template('index.html', prediction=f'Error: Only {allowed_extensions_str} files are allowed')
+
+    try:
+        # Read the image file into memory
+        image_stream = file.stream
+        # Load the image from the stream
+        image = load_img(BytesIO(image_stream.read()), target_size=(224, 224))
+        # Preprocess the image
+        image = preprocess_image(image)
+        # Make predictions
+        predictions = model.predict(image)
+        predicted_class = np.argmax(predictions)
+
+        # Convert image to base64 string
+        buffered = BytesIO()
+        array_to_img(image[0]).save(buffered, format="JPEG")
+        image_data = base64.b64encode(buffered.getvalue()).decode("utf-8")
+
+        # Return the result
+        return render_template('index.html', prediction=(labels[predicted_class]).capitalize(), uploaded_image=image_data)
+    except UnidentifiedImageError as e:
+        return render_template('index.html', prediction=f'Error: Unable to identify the image file. {e}')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
