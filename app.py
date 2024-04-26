4de11cfc6051a5e461a9a9cf604ed4ef690e1597
HanaHelaly <hanahelaly99@gmail.com>
2024-04-26 22:25:59 +0300

    uploading another weather app

--- app.py
+++ app.py
@@ -1,27 +1,10 @@
 from flask import Flask, render_template, request
-from base64 import b64encode
-from PIL import Image
+import base64
+from tensorflow.keras.preprocessing.image import load_img, img_to_array, array_to_img
 from tensorflow.keras.models import load_model
 import numpy as np
 from io import BytesIO
 from PIL import UnidentifiedImageError
-import os
-
-
-
-destination = 'model/model.h5'
-
-if os.path.exists('model/'):
-    print("model found")
-    pass
-else:
-    print("model not found, Downloding model.....")
-    import gdown
-    url = 'https://drive.google.com/uc?id=15sGlZvK-TcuSMqIvgT4OdguXC6aocMYr'
-    os.makedirs('model', exist_ok=True)
-    gdown.download(url, destination, quiet=False)
-
-
 
 labels = {
     0: 'dew',
@@ -37,17 +20,19 @@ labels = {
     10: 'snow'
 }
 
-
-
 app = Flask(__name__)
+allowed_extensions = ['.jpg', '.jpeg', '.png']
 
 # Load the model once during application startup
-model = load_model(destination)
+model = load_model('./model/FINAL_EfficientNetB7-201.h5')
 
 def preprocess_image(image):
-    image = image.resize((224, 224))
-    image = np.array(image) / 255.
+    image = img_to_array(image) / 255.
     return np.expand_dims(image, axis=0)
+
+def close_streams(streams):
+    for stream in streams:
+        stream.close()
 
 @app.route('/')
 def index():
@@ -57,25 +42,26 @@ def index():
 def predict():
     try:
         file = request.files['image']
-        if not any(file.filename.lower().endswith(ext) for ext in ['.jpg', '.jpeg', '.png']):
-            allowed_extensions_str = ', '.join(['.jpg', '.jpeg', '.png'])
+        if not any(file.filename.lower().endswith(ext) for ext in allowed_extensions):
+            allowed_extensions_str = ', '.join(allowed_extensions)
             return render_template('index.html', prediction=f'Error: Only {allowed_extensions_str} files are allowed')
 
-        image = Image.open(file)
+        image_stream = file.stream
+        image = load_img(BytesIO(image_stream.read()), target_size=(224, 224))
+        image_stream.seek(0)  # Reset stream position to beginning
         image_data = None  # Initialize image data
 
         try:
             image = preprocess_image(image)
             predicted_class = np.argmax(model.predict(image))
             buffered = BytesIO()
-            Image.fromarray((image[0] * 255).astype(np.uint8)).save(buffered, format="JPEG")
-            image_data = b64encode(buffered.getvalue()).decode("utf-8")
+            array_to_img(image[0]).save(buffered, format="JPEG")
+            image_data = base64.b64encode(buffered.getvalue()).decode("utf-8")
             return render_template('index.html', prediction=labels[predicted_class].capitalize(), uploaded_image=image_data)
         except UnidentifiedImageError as e:
             return render_template('index.html', prediction=f'Error: Unable to identify the image file. {e}')
         finally:
-            file.close()  # Close file stream
-            buffered.close()  # Close buffer
+            close_streams([image_stream, buffered])  # Close streams to release resources
 
     except Exception as e:
         return render_template('index.html', prediction=f'Error: {e}')
